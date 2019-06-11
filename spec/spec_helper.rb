c929f1151ef0b43ff262eba84540c8a0773c3f68
Naomi Dennis <njdennis94@gmail.com>
2019-06-11 16:40:12 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,7 @@ RSpec.configure do |config|
       to_return(:status => 200, :body => [{"name" => "Repo 1", "html_url" => "http://link1.com"}, {"name" => "Repo 2", "html_url" => "http://link2.com"}, {"name" => "Repo 3", "html_url" => "http://link3.com"}].to_json, :headers => {})
 
     
-   stub_request(:post, "https://github.com/login/oauth/access_token?client_id=#{ENV["client_id"]}&client_secret=#{ENV["client_secret"]&code=20").
+   stub_request(:post, "https://github.com/login/oauth/access_token?client_id=#{ENV["client_id"]}&client_secret=#{ENV["client_secret"]}&code=20").
          with(
            headers: {
           'Accept'=>'*/*',
